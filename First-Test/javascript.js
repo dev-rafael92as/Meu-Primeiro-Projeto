d283cb7822319ac25fd0648c18d570c8596e3077
dev-rafael92as <77762757+dev-rafael92as@users.noreply.github.com>
2021-01-28 02:47:19 -0300

    Finalização - Analizador de Números

--- First-Test/javascript.js
+++ First-Test/javascript.js
@@ -1,33 +1,48 @@
-ArrayNumeros = [0]
+ArrayNumeros = []
 
 function analisarNumero() {
-  let num = document.getElementById('numero').value
-  document.getElementById('numero').value = ''
-
-    
-  if (num.length === 0) {
-    alert('Digite um valor valido para Adicionar!')
+  //Lógica para pegar o número digitado pelo usuário
+  let num = document.getElementById('numero').value;
+  //Apagar caixa de texto após a variavel for salva
+  document.getElementById('numero').value = '';
+  //Tratar o tipo de variável para Number (Caso seja uma letra irá retornar o valor NaN)
+  num = parseFloat(num);
+  
+  //Lógica para apenas aceitar incluir na lista numeros válidos
+  if (Number.isNaN(num)) {
+    alert('Digite um número!');
+  } else if (num.length === 0) {
+    alert('Digite um valor valido para Adicionar!');
   } else if (ArrayNumeros.indexOf(num) > -1) {
-    alert('Este Valor já foi Adicionado, tente outro!')
+    alert('Este Valor já foi Adicionado, tente outro!');
   } else if (num > 100) {
-    alert('Digite um valor abaixo de 100!')
-  } else if (num < 0) {
-    alert('Digite um valor maior que zero!')
+    alert('Digite um valor abaixo de 100!');
+  } else if (num <= 0) {
+    alert('Digite um valor maior que zero!');
   } else {
-    ArrayNumeros.push(num)
+    document.getElementById('listaResultados').innerHTML = '';
+    ArrayNumeros.push(num);
     let x = document.getElementById("flista");
     let option = document.createElement("option");
-    option.text = 'Número: ' + num + ' adicionado'
+    option.text = 'Número: ' + num + ' adicionado';
     x.add(option);    
   }
-  
 }
 
 function finalizarAplicacao() {
-  console.log(ArrayNumeros)
-  ArrayNumeros.forEach(function(valor, indice, array) {
-    
-    let a = 0
-    a += valor 
-    document.getElementById('listaResultados').innerHTML = a})
-  }
+    let soma = ArrayNumeros.reduce((soma, num) => soma + num, 0);
+
+  ArrayNumeros.sort(function(a, b){return b - a});
+  let maiorNumero = ArrayNumeros[0];
+
+  ArrayNumeros.sort(function(a, b){return a - b});
+  let menorNumero = ArrayNumeros[0];
+
+  let quantNumeros = ArrayNumeros.length;
+  let mediaNumeros = soma / quantNumeros;
+
+    document.getElementById('listaResultados').innerHTML = `<hr>A soma de todos os valores é ${soma}! <br><hr>
+    O maior número é ${maiorNumero}! <br><hr>
+    O menor número é ${menorNumero}!<br><hr>
+    A média dos números é ${mediaNumeros}!`
+}
